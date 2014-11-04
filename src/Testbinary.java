be8d9006ea29426d7ab1f92594b3eca328a21fd3
William <agohwill@gmail.com>
2014-11-03 21:34:18 -0500

    Program after cleaning up my code

--- src/Testbinary.java
+++ src/Testbinary.java
@@ -3,39 +3,42 @@ import java.util.Scanner;
 
 public class Testbinary {
 
-	/**
-	 * This program prompt a user to enter a binary number and convert it to it 
-	 * corresponding decimal value.
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Scanner in = new Scanner(System.in);
-		 
-	    System.out.print("Enter a binary number --->: ");
-	    String binary = in.nextLine();
-	      
-	    String decimal = " ";
-		int product = 1; // Initialize product to 1 power to 1 and result to 0 in order 
-						// to process the result
-		int power = 1;
-		int result = 0; 
-		
-		// go through the string and process the last binary number
-		for (int i = binary.length()-1; i>=0;i--){
-			
-			if (binary.charAt(i) =='1'){	// if the character is 1 
-				result += product;
-				product = product *2; // get the power of two 
-			}
-			else { // else 
-				power = 0; // add the zero to the final result
-				result += power;
-				product = product *2;
-				}
-			decimal = result + ""; // append the result to be displayed 
-		}
-		System.out.println(decimal);
+
+		Scanner userInput = new Scanner(System.in);
+		String binary;
+		int decimalNumber;
+
+		System.out.print("Enter a binary number --->: ");
+		binary = userInput.nextLine();
+
+		decimalNumber = BinaryToDecimalConverter(binary);
+
+		System.out.printf("The decimal number of %s is %d",binary,decimalNumber);
 	}
-	
+
+	public static int BinaryToDecimalConverter(String Binarynumber){
+
+		String binary = Binarynumber;
+		int nextPowerOfTwo = 1;
+		int currentPowerofTwo = 1;
+		int decimalNumber = 0; 
+
+		for (int i = binary.length()-1; i>=0;i--){
+
+			if (binary.charAt(i) =='1')
+			{
+				currentPowerofTwo = nextPowerOfTwo;
+				decimalNumber += currentPowerofTwo;
+				nextPowerOfTwo *= 2; 
+			}
+			else{ 
+				decimalNumber += 0;
+				nextPowerOfTwo *= 2;
+			}
+		}
+		return decimalNumber;
+	}
+
 }
